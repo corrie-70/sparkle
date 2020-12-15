06ceb2895e70460e56f070457be5ce9195ddfa0f
huangke <huangke@skieer.cn>
2020-12-15 16:56:22 +0800

    less添加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-import './App.css';
 
 import { Home } from './pages/home';
 import { About } from './pages/about';
