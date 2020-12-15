25e5b2a1c7b2c3d42a66fae42c2dd4d75849d640
huangke <huangke@skieer.cn>
2020-12-15 16:27:16 +0800

    antd 按需加载

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+
+const About = () => {
+    useEffect(() => { }, []);
+    return <div>about</div>;
+};
+
+export { About };
---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import { Button, message } from 'antd';
+import React from 'react';
+
+const Home = () => {
+    return <div>Home
+        <Button onClick={() => { message.warn('点击了信息') }}>点击</Button>
+    </div>;
+};
+
+export { Home };
