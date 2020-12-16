ce7ea4ccbde653150e2525085df824f1f9ec1758
huangke <huangke@skieer.cn>
2020-12-16 10:51:26 +0800

    main render文件夹区分，main打包工程化待实现，目前需要手动执行命令

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,21 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const path = require('path');
 
-module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true
-    }),
-    addLessLoader({
-        lessOptions: {
-            javascriptEnabled: true
-        }
-    })
-);
+module.exports = {
+    webpack: override(
+        fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true
+        }),
+        addLessLoader({
+            lessOptions: {
+                javascriptEnabled: true
+            }
+        })
+    ),
+    paths: (paths) => {
+        paths.appIndexJs = path.resolve(__dirname, 'src/renderer/index.tsx');
+        return paths;
+    }
+};
