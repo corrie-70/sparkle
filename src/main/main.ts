7547feb04ae2482f46c46334a84457a619d8abfd
corrie <kehuang1013@gmail.com>
2021-06-17 17:38:52 +0800

    软件更新相关

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,8 +3,6 @@ import { appWindow } from "./window";
 const electron = require("electron");
 const { app } = electron;
 
-// export {};
-
 (function () {
     app.whenReady().then(appWindow.createWindow);
 })();
